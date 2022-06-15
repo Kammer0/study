b4b52cd49d48e20181e54118603ff29e0bfa23d6
Kammer0 <107503946+Kammer0@users.noreply.github.com>
2022-06-15 20:49:54 -0300

    Create README.py

---
+++ README.py
@@ -0,0 +1,33 @@
+# study
+
+
+Enunciado del ejercicio:
+
+Para este ejercicio tenéis que crear en la consola de python variables que representen los siguientes datos de un contacto:
+
+nombre = "Santiago"
+
+apellido = "Kemerer"
+
+edad = 24
+
+email = "kemerer381@gmail.com"
+
+teléfono = +542257541409
+
+casado = False
+
+hijos = False
+
+list_friends = ["Alan", "Alexis", "Marcos"]
+
+pelis_vistas = { 1: "Harry Potter",
+                 2: "Star Wars"
+                 3: "El señor de los anillos"
+}
+
+print ("Nombre Completo:", nombre, apellido, "\n", 
+       "Edad:", edad, "\n", "Mail:, email, "\n",
+       "Tel:", teléfono, "\n", "Casado:", casado, "\n",
+       "Hijos:", hijos, "\n", "Lista de amigos:", list_friends,
+       "\n", "Peliculas:", pelis_vistas)
